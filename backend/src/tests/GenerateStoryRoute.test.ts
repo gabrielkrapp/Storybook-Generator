08cdec29bcb190291cf426c0fcf4e2e1123b9157
Gabriel <gabriel.krapp@hotmail.com>
2023-11-24 21:29:16 -0300

    Adding Unit and Integration Tests

--- backend/src/tests/GenerateStoryRoute.test.ts
+++ backend/src/tests/GenerateStoryRoute.test.ts
@@ -1,36 +1,45 @@
 import request from 'supertest';
 import express from 'express';
-import router from '../routers/GenerateStory';
 import bodyParser from 'body-parser';
+import generatestory from '../routers/GenerateStory';
 import axios from 'axios';
+
+jest.mock('axios');
 
 const app = express();
 app.use(bodyParser.json());
-app.use('/generate-story', router);
+app.use(generatestory);
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('POST /generate-story', () => {
   it('should generate a story based on the provided theme', async () => {
+
     const theme = 'dragão amigável';
+    mockedAxios.post.mockResolvedValueOnce({
+      data: {
+        choices: [{
+          text: "Era uma vez, em um reino muito distante, uma pequena vila cercada por montanhas..."
+        }]
+      }
+    });
 
     const res = await request(app)
       .post('/generate-story')
-      .send({ theme: theme });
+      .send({ theme });
 
     expect(res.status).toBe(200);
     expect(res.body).toHaveProperty('story');
+    expect(res.body.story).toContain('montanhas');
   });
 
-  it('should handle OpenAI API errors gracefully', async () => {
+  it('should respond with status 500 when an error occurs', async () => {
     const theme = 'dragão amigável';
-
-    (axios.post as jest.Mock).mockRejectedValueOnce(new Error('OpenAI API is down'));
+    mockedAxios.post.mockRejectedValueOnce(new Error('Erro ao processar sua solicitação'));
 
     const res = await request(app)
-      .post('/api/generate-story')
+      .post('/generate-story')
       .send({ theme });
 
     expect(res.status).toBe(500);
-    expect(res.body).toHaveProperty('error');
   });
-
 });
