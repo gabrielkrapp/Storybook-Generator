4c2eb67467504615529ee31296370e38c39bfcae
Gabriel <gabriel.krapp@hotmail.com>
2023-11-26 19:34:12 -0300

    excluding swagger

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,14 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import { limiter } from './config/RateLimit';
 import generatestory from './routers/GenerateStory';
-import swaggerUi from 'swagger-ui-express';
-import swaggerSpec from './swagger/swagger';
+// import swaggerUi from 'swagger-ui-express';
+// import swaggerSpec from './swagger/swagger';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(limiter);
 app.use(generatestory);
 
