b36474599c75dcd89a3042752f93362a389331b5
Gabriel <gabriel.krapp@hotmail.com>
2023-11-26 20:07:41 -0300

    adding / route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,14 +2,17 @@ import express from 'express';
 import cors from 'cors';
 import { limiter } from './config/RateLimit';
 import generatestory from './routers/GenerateStory';
-// import swaggerUi from 'swagger-ui-express';
-// import swaggerSpec from './swagger/swagger';
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from './swagger/swagger';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get('/', (req, res) => {
+    res.send('Hey this is my API running 🥳');
+});
 app.use(limiter);
 app.use(generatestory);
 
